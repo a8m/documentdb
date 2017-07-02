1ecfbbdf5f197e8b3066c58880b2c081d1e76b1f
snobu <foo@snobu.org>
2017-07-02 16:12:28 +0300

    Handle malformed CosmosDB key

--- auth.go
+++ auth.go
@@ -4,13 +4,19 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+        "errors"
 )
 
 func authorize(str, key string) (string, error) {
 	var ret string
 	enc := base64.StdEncoding
 	salt, err := enc.DecodeString(key)
+
 	if err != nil {
+		if _, ok := err.(base64.CorruptInputError); ok {
+                        err = errors.New("base64 input is corrupt, check CosmosDB key.")
+			return ret, err
+		}
 		return ret, err
 	}
 	hmac := hmac.New(sha256.New, salt)
