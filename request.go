c0a1924a89eebd5c9fa8443f6e306de8ee553739
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2022-03-24 11:00:15 -0700

    Allow using a link with IDs rather than RID's

--- request.go
+++ request.go
@@ -125,12 +125,30 @@ func parse(id string) (rId, rType string) {
 	l := len(parts)
 
 	if l%2 == 0 {
-		rId = parts[l-2]
 		rType = parts[l-3]
 	} else {
-		rId = parts[l-3]
 		rType = parts[l-2]
 	}
+
+	// Check if we're being passed a _self link or a link that uses IDs
+	// If we have a self link, parts[2] should be a 6-byte, base64-encoded string, that is 8 characters long and includes padding ("==")
+	// "=" is not a valid character in a Cosmos DB identifier, so if we notice that (especially in a string that's 8-chars long), we know it's a RID
+	if l > 3 && len(parts[2]) == 8 && parts[2][6:] == "==" {
+		// We have a _self link
+		if l%2 == 0 {
+			rId = parts[l-2]
+		} else {
+			rId = parts[l-3]
+		}
+	} else {
+		// We have a link that uses IDs
+		end := l - 1
+		if l%2 == 1 {
+			end = l - 2
+		}
+		rId = strings.Join(parts[1:end], "/")
+	}
+
 	return
 }
 
